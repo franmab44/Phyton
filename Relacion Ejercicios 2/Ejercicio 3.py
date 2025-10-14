fff4347269a47e4ab3dfd47eeab6bd024c09315c
Francisco Mateos <fmatbuf1211@ieszaidinvergeles.org>
2025-10-14 13:24:03 +0200

    .

--- Relacion Ejercicios 2/Ejercicio 3.py
+++ Relacion Ejercicios 2/Ejercicio 3.py
@@ -1,2 +1,2 @@
 nombre = input("Introduce tu nombre de usuario: ")
-print(f"{nombre}, tiene{len(nombre)} letras, donde {(nombre)} es el nombre de usuario")
+print(f"{nombre}, tiene {len(nombre)} letras, donde {(nombre)} es el nombre de usuario")
