7bb35492e8bea564ad001dc4bca6bfeb147657dc
Francisco Mateos <fmatbuf1211@ieszaidinvergeles.org>
2025-11-04 13:54:53 +0100

    Ejercicio 3

---
+++ Relación Ejercicios 3/Ejercicio 3.py
@@ -0,0 +1,6 @@
+numero1 = input("Ingrese numero uno: ")
+numero2 = input("Ingrese numero dos: ")
+calculo = float(numero1) / float(numero2)
+print("El resultado de la division es: ", calculo)
+if numero2 == "0":
+    print("Error rrr rrr error")
