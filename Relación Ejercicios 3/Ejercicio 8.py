07f1aa8cdc6aaa335c80426c84386ac103e37d4c
Francisco Mateos <fmatbuf1211@ieszaidinvergeles.org>
2025-11-11 13:52:36 +0100

    Ejercicio 8

---
+++ Relación Ejercicios 3/Ejercicio 8.py
@@ -0,0 +1,19 @@
+Valor = 2400
+
+puntuacion = float(input("Introduce la puntuación del empleado: "))
+
+nivel = ""
+if puntuacion == 0.0:
+    nivel = "Mal"
+else:
+    if puntuacion == 0.4:
+        nivel = "Bien"
+    else:
+        if puntuacion >= 0.6:
+            nivel = "Muy Bien"
+
+cantidaddinero = Valor * puntuacion
+
+print("Puntuación:", puntuacion)
+print("Rendimiento:", nivel)
+print("Cantidad a recibir:", cantidad_dinero, "€")
